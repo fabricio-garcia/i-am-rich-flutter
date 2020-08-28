68fe23f34a04e754af68ea958f9968d0b23cf847
Fabricio Garcia <gael.fabricio@gmail.com>
2020-08-28 17:08:35 -0500

    feat: Add content to the body
    - Add Center widget
    - Add Image as child of Center widget
    - Add AssetImage widget as param in Image widget
    - Created images folder
    - Modify pubspec.yaml to include image folder as assets source and pub getted

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: Text('I am rich'),
         centerTitle: true,
         backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
       ),
     ),
   ));
